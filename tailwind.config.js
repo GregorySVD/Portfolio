1739ac4779be2443db298eb735d65ff094da7467
GregorySVD <grzegorzterenda@gmail.com>
2024-09-22 17:06:07 +0200

    Configurate Css and tailwind Css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
+const { main } = require('framer-motion/client');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        bg: {
+          main: '#111111',
+          dark: '#080808',
+        },
+        text: {
+          main: '#ebecf3',
+        },
+        accent: '#12ff9d',
+      },
+    },
   },
   plugins: [],
 };
