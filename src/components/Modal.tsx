55428cbaa8013987cc4b876661fd6fcc9b3f2283
GregorySVD <grzegorzterenda@gmail.com>
2024-09-30 20:11:56 +0200

    feat: create modal footer

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,7 @@
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect } from 'react';
+import { ModalFooter } from './ModalFooter';
 
 interface ModalProps {
   isOpen: boolean;
@@ -10,9 +11,21 @@ interface ModalProps {
   alt: string;
   cardMainImg: string;
   stack: string;
+  deploymentLink: string;
+  repoUrl: string;
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, projectDescription, alt, cardMainImg, stack }) => {
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  title,
+  projectDescription,
+  alt,
+  cardMainImg,
+  stack,
+  deploymentLink,
+  repoUrl,
+}) => {
   useEffect(() => {
     if (isOpen) {
       document.body.style.overflow = 'hidden';
@@ -29,18 +42,24 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, projectDescriptio
 
   return (
     <div
-      className="fixed top-0 left-0 right-0 z-50 w-full h-full py-12 px-1 backdrop-blur-[12px] flex justify-center cursor-pointer"
+      className="fixed top-0 left-0 right-0 z-50 w-full h-full py-12 px-1 backdrop-blur-[12px] flex justify-center items-center cursor-pointer"
       onClick={() => onClose()}
     >
       <button onClick={onClose} className="absolute top-2 right-2 text-2xl">
         <FontAwesomeIcon icon={faTimes} />
       </button>
-      <div className="bg-bg-chip rounded-lg max-w-700px mx-auto relative cursor-default">
-        <img src={cardMainImg} alt={alt} className=" w-full h-1/2 object-cover object-top rounded-t-xl" />
-        <div className="px-6">
-          <h2 className="text-2xl font-bold mb-4">{title}</h2>
-          <div className="relative w-fit overflow-hidden text-accent my-5">{stack}</div>
-          <p className="text-zinc-300">{projectDescription}</p>
+      <div
+        className="bg-bg-chip rounded-xl max-w-700px min-w-[300px] w-full max-h-[90vh] overflow-y-auto mx-auto relative cursor-default flex flex-col hide-scrollbar"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <img src={cardMainImg} alt={alt} className="w-full h-[50vh] object-cover object-top rounded-t-xl" />
+        <div className="px-6 py-4 flex flex-col flex-grow justify-between">
+          <div className="flex-grow overflow-y-auto">
+            <h2 className="text-3xl font-bold mb-4">{title}</h2>
+            <div className="relative w-fit overflow-hidden text-accent my-5">{stack}</div>
+            <p>{projectDescription}</p>
+          </div>
+          <ModalFooter deploymentLink={deploymentLink} repoUrl={repoUrl} />
         </div>
       </div>
     </div>
