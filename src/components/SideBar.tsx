763bf008adb56fc5b8dd1cfd0c5e3747008e1d9c
GregorySVD <grzegorzterenda@gmail.com>
2024-10-14 20:37:28 +0200

    update: fixed positioning if side bar over header

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -2,8 +2,8 @@ import { SideBarLink } from './SideBarLink';
 
 export const SideBar = () => {
   return (
-    <nav className="bg-bg-dark h-screen sticky top-0 left-0 z-20 flex flex-col items-center scrollbar-hide min-w-[64px] flex-shrink-0">
-      <div className="w-11 h-11 bg-bg-main text-2xl font-extrabold flex items-center justify-center my-4 border-radius rounded-md">
+    <nav className="bg-bg-dark h-screen sticky top-0 left-0 z-30 flex flex-col items-center min-w-[64px]">
+      <div className="w-11 h-11 bg-bg-main text-2xl font-extrabold flex items-center justify-center my-4 rounded-md">
         <span className="text-center align-text-bottom">G</span>
         <span className="text-accent">.</span>
       </div>
@@ -11,6 +11,7 @@ export const SideBar = () => {
       <SideBarLink text="Projects" goToSectionId="projects" />
       <SideBarLink text="Exp" goToSectionId="exp" />
       <SideBarLink text="Contact" goToSectionId="contact" />
+      <div className="flex-grow"></div>
     </nav>
   );
 };
