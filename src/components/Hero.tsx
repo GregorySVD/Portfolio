0d2f2c4d4efb8646769904035f3afe5448c850da
GregorySVD <grzegorzterenda@gmail.com>
2024-10-24 20:55:38 +0200

    Update: - Implemented Lottie animation for arrow pointing to the contact button - Fixed hover effect clipping on the left side of the contact button - Corrected scroll behavior for "Contact Me" button to smoothly navigate to the contact section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,6 @@
+import { MouseEventHandler } from 'react';
 import { TypingAnimation } from './TypingAnimation';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 export const Hero = () => {
   const options = [
@@ -8,9 +10,16 @@ export const Hero = () => {
     'Computer Science Student',
   ];
 
+  const goToSectionId: MouseEventHandler<HTMLButtonElement> = () => {
+    const section = document.getElementById('contact');
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className="my-12">
-      <div className="text-left py-4">
+      <div className="text-left p-4">
         <h1 className="text-6xl md:text-7xl font-extrabold mb-4">
           Hey, I'm Grzegorz<span className="text-accent">.</span>
         </h1>
@@ -23,9 +32,20 @@ export const Hero = () => {
           by space exploration 🚀 pushing the boundaries of technology beyond our planet is just as thrilling as
           building innovative software here on Earth. Currently, I’m proudly pursuing a degree in Computer Science 💻
         </p>
-
-        <p className="text-xl mt-5">Let's connect 💫</p>
-        <button className="hover:animate-jump animate-once inline-flex items-center justify-center p-0.5 my-5 me-2 bg-accent text-bg-main overflow-hidden text-sm font-medium rounded-md px-5 py-2.5 group bg-gradient-to-br from-transparent to-transparent hover:from-accent hover:to-accent transition-all duration-500 border border-accent">
+        <div className="relative">
+          <p className="text-2xl mt-5">Let's connect 💫</p>
+          <DotLottieReact
+            src="/Arrow.lottie"
+            loop
+            autoplay
+            style={{ width: '12rem', height: '12rem', rotate: '0deg', background: 'red' }}
+            className="absolute left-28 -top-2 md:left-28 md:-top-8 "
+          />
+        </div>
+        <button
+          className="hover:animate-jump animate-once inline-flex items-center justify-center p-0.5 my-5 me-2 bg-accent text-bg-main overflow-hidden text-xl font-medium rounded-md px-5 py-2.5 group bg-gradient-to-br from-transparent to-transparent hover:from-accent hover:to-accent transition-all duration-500 border border-accent"
+          onClick={goToSectionId}
+        >
           Contact Me
         </button>
       </div>
